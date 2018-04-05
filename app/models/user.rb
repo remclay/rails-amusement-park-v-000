71245415818290fade90bd1f016b88493f88af6a
remclay <remmy.lauren@gmail.com>
2018-04-04 21:58:56 -0600

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
-  # write associations here
+  validates :password, presence: true
+  has_many :rides
+  has_many :attractions, through: :rides
+  validates_inclusion_of :admin, :in => [true, false]
+  has_secure_password
+
+  def mood
+    if self.happiness >= self.nausea
+      "happy"
+    else
+      "sad"
+    end
+  end
 end
