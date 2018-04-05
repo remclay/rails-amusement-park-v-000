5915537daf3368969de82096f07823f430bd0016
remclay <remmy.lauren@gmail.com>
2018-04-04 22:25:52 -0600

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -7,7 +7,7 @@ class Ride < ActiveRecord::Base
   def take_ride
     @user = self.user
     @attraction = self.attraction
-    
+
     if enough_tickets && correct_height
         ride_attraction
     elsif !enough_tickets && !correct_height
@@ -20,21 +20,22 @@ class Ride < ActiveRecord::Base
   end
 
   def enough_tickets
-    attraction.tickets < @user.tickets
+    attraction.tickets <= @user.tickets
   end
 
   def correct_height
-    attraction.min_height < @user.height
+    attraction.min_height <= @user.height
   end
 
   def ride_attraction
+    # binding.pry
     @user.tickets = @user.tickets - @attraction.tickets
     @user.nausea = @user.nausea + @attraction.nausea_rating
     @user.happiness = @user.happiness + @attraction.happiness_rating
 
     # user.update(:happiness => happiness_rating, :nausea => nausea_rating, :tickets => ticket_balance)
     @user.save
-    # binding.pry
+
     # "Thanks for riding the #{self.attraction.name}!"
   end
 
